9dd0480617fff3922396f64bdb58e81cf1d02460
umiumiu25 <20010129kk@gmail.com>
2024-10-27 20:20:55 +0900

    ボタンコンポーネントを3種類定義
    ・その他: スタイル調整と軽微な修正

---
+++ src/components/button/Button1.tsx
@@ -0,0 +1,24 @@
+import styles from "@/styles/Button.module.css";
+import { ButtonProps } from "@/types/button.types";
+import Button from "./Button";
+
+const Button1: React.FC<ButtonProps> = ({
+  type,
+  label,
+  onClick,
+  addButtonStyle,
+  Icon,
+}) => {
+  return (
+    <Button
+      type={type}
+      label={label}
+      onClick={onClick}
+      className={styles.button1}
+      addButtonStyle={addButtonStyle}
+      Icon={Icon}
+    />
+  );
+};
+
+export default Button1;
